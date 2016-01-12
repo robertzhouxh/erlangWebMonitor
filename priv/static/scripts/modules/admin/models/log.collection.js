fec70ce809d1171a4a170622d0746bbae4435807
huangguozhen <huangguozhen@outlook.com>
2016-01-12 11:39:15 +0800

    show priority level colorful

---
+++ priv/static/scripts/modules/admin/models/log.collection.js
@@ -0,0 +1,17 @@
+define([
+],
+
+function(){
+    'use strict';
+
+    var logModel = Backbone.Model.extend({
+        urlRoot: 'v2/logs',
+    });
+
+    var logCollection = Backbone.Collection.extend({
+        url: 'v2/logs',
+        model: logModel,
+    });
+
+    return logCollection;
+});
--- priv/static/scripts/modules/admin/views/openresty.view.js
+++ priv/static/scripts/modules/admin/views/openresty.view.js
@@ -1,28 +1,67 @@
 define([
     'hbs!./templates/openresty',
-    //'hbs!./templates/openresty/composite',
-    //'hbs!./templates/openresty/item',
+    'hbs!./templates/openresty/composite',
+    'hbs!./templates/openresty/item',
+    '../../admin/models/log.collection',
     'datatables.bootstrap',
 ],
 
-function(tmplOpenresty){
+function(tmplOpenresty, tmplComposite, tmplItem, LogCollection){
     'use strict';
 
-    //var LogItemView = Marionette.ItemView.extend({
-        //tagName: 'tr',
-        //template: tmplItem,
-    //});
+    var LogItemView = Marionette.ItemView.extend({
+        tagName: 'tr',
+        template: tmplItem,
+        templateHelpers: function() {
+            var severity = '';
+            switch (this.model.get('SEVERITY_NUM')) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    severity = 'label-danger';
+                    break;
+                case 4:
+                    severity = 'label-warning';
+                    break;
+                case 5:
+                case 6:
+                    severity = 'label-info';
+                    break;
+                case 7:
+                    severity = 'label-primary';
+                    break;
+                default:
+                    severity = 'label-primary';
+                    break;
+            }
 
-    //var LogCompositeView = Marionette.CompositeView.extend({
-        //tagName: 'div',
-        //className: 'col-xs-12',
-        //template: tmplComposite,
-        //childView: LogItemView,
-        //childViewContainer: 'tbody',
-        //ui: {
-            //table: '.table',
-        //},
-    //});
+            return { level: severity };
+        }
+    });
+
+    var LogCompositeView = Marionette.CompositeView.extend({
+        tagName: 'div',
+        className: 'col-xs-12',
+        template: tmplComposite,
+        childView: LogItemView,
+        childViewContainer: 'tbody',
+        ui: {
+            table: '.table',
+        },
+
+        initialize: function() {
+            var that = this;
+            this.listenTo(this.collection, 'error', syncError);
+            that.listenTo(that.collection, 'update', function() {
+                that.render();
+            });
+        },
+
+        onRender: function() {
+            this.ui.table.DataTable();
+        },
+    });
 
     var openrestyView = Marionette.LayoutView.extend({
         template: tmplOpenresty,
@@ -32,16 +71,20 @@ function(tmplOpenresty){
             description: 'Web server log',
         },
 
-        ui: {
-            table: '.table',
+        regions: {
+             'content': '.content .row',
         },
 
         onShow: function() {
-            this.ui.table.DataTable({
-                "ajax": "v2/logs"
-            });
+            var logs = new LogCollection();
+            this.showChildView('content', new LogCompositeView({collection: logs}));
+            logs.fetch();
         },
     });
+
+    function syncError(model, resp, options) {
+        Backbone.history.navigate('', {trigger: true});
+    }
 
     return openrestyView;
 });
